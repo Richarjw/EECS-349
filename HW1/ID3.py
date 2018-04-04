d001f07a35c639ce0fba8d22b9d7bb213dfb43e4
Nico <ntyjeski@gmail.com>
2018-04-04 18:20:16 -0500

    HW1

---
+++ HW1/ID3.py
@@ -0,0 +1,41 @@
+from node import Node
+from parse import parse
+import math
+
+Data = parse("/Users/Nico/Documents/GitHub/EECS-349/house_votes_84.data")
+
+def ID3(examples, default):
+  '''
+  Takes in an array of examples, and returns a tree (an instance of Node) 
+  trained on the examples.  Each example is a dictionary of attribute:value pairs,
+  and the target class variable is a special attribute with the name "Class".
+  Any missing attributes are denoted with a value of "?"
+  '''
+  if examples == {}:
+      return Node()
+  elif "all examples have same classification or no non-trivial splits are available":
+      return Node
+  
+         
+def prune(node, examples):
+  '''
+  Takes in a trained tree and a validation set of examples.  Prunes nodes in order
+  to improve accuracy on the validation data; the precise pruning strategy is up to you.
+  '''
+
+def test(node, examples):
+  '''
+  Takes in a trained tree and a test set of examples.  Returns the accuracy (fraction
+  of examples the tree classifies correctly).
+  '''
+
+
+def evaluate(node, example):
+  '''
+  Takes in a tree and one example.  Returns the Class value that the tree
+  assigns to the example.
+  '''
+
+x = Node()
+x.children = {"billy","joe"}
+
